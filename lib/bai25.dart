fc40eaa20ac7de9f2497bf301daca3258b2f4b83
DuongPhan99 <83325564+Duongphan99@users.noreply.github.com>
2022-11-06 15:40:33 +0700

    my changes

--- lib/bai25.dart
+++ lib/bai25.dart
@@ -1,1 +1,16 @@
 //Bài 25: Tính tổng tất cả các ước số chẵn của số nguyên dương n
+void main() {
+  print(tinhSoNguyenDuong(20));
+}
+
+int tinhSoNguyenDuong(int n) {
+  int sum = 0;
+  for (int i = 1; i <= n; i++) {
+    if (n % i == 0) {
+      if (i % 2 == 0) {
+        sum = sum + i;
+      }
+    }
+  }
+  return sum;
+}
